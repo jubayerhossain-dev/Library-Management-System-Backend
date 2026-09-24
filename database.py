3770b278a17cb2ca478592521632f545e9297ea6
jubayerhossain0 <jubayercitdm@gmail.com>
2026-09-24 22:19:01 +0600

    update sqlite

--- database.py
+++ database.py
@@ -4,6 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQL_DATABASE_URL = 'postgresql://postgres.omhtvvbjuwbmztgxcodu:Jubayer00912@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
-engine = create_engine(SQL_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQL_DATABASE_URL)
 sessionlocal = sessionmaker(autoflush= False, autocommit= False, bind=engine)
 Base = declarative_base()
