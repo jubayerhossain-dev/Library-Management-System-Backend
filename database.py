409ee49a4c9b05ff852e61061875a211144ed9f0
jubayerhossain0 <jubayercitdm@gmail.com>
2026-09-24 22:13:35 +0600

    spabase add

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_DATABASE_URL = 'sqlite:///./DataCenterHome.db'
+SQL_DATABASE_URL = 'postgresql://postgres.omhtvvbjuwbmztgxcodu:Jubayer00912@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
 engine = create_engine(SQL_DATABASE_URL, connect_args={'check_same_thread': False})
 sessionlocal = sessionmaker(autoflush= False, autocommit= False, bind=engine)
